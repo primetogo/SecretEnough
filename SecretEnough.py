6ba43f054358185ef44c6c571fc67b3462ed31d1
Panupong Prueksa <s5070089@kmitl.ac.th>
2014-11-27 21:34:26 +0700

    Big change to coding
    Here i come!!!! booom!

--- SecretEnough.py
+++ SecretEnough.py
@@ -1,29 +1,63 @@
-"""Project PSIT"""
+"""How was your password?"""
 from Tkinter import *
 
 class Application(Frame):
     def createWidgets(self):
-        L1 = Label(self, text="Insert Password")
-        L1.grid(row=0, column=0)
-        E1 = Entry(self, bd=5)
-        E1.focus_set()
-        E1.grid(row=0, column=1)
-        B1 = Button(self, text="OK")
-        B1.grid(row=0, column=2)
-        B1["command"] = self.printresult
-        
-    def printresult(self):
-        print "Your Password is normally secret"
-        
+        label_input = Label(self, text="Insert Password")
+        label_input.grid(row=0, column=0)
+        pass_input = Entry(self, bd=5)
+        pass_input.focus_set()
+        pass_input.grid(row=0, column=1)
+        btn_check = Button(self, text="OK")
+        btn_check.grid(row=0, column=2)
+        btn_check["command"] = self.basic_data
+        #cannot operate need to pass password input
+        #from pass_input to basic data
+
+    def basic_data(self): #calculate inside password
+        upper = 0
+        lower = 0
+        symbols = 0
+        number = 0
+        get_pass = pass_input.get()
+        for element in get_pass:
+            if element.isalpha():
+                if element.isupper():
+                    upper += 1
+                else:
+                    lower += 1
+            elif element.isdigit():
+                number += 1
+            else:
+                symbols += 1
+        if upper == 0:
+            upper = "No upper case"
+        elif lower == 0:
+            lower = "No lower case"
+        elif symbols == 0:
+            symbols = "No symbols"
+        elif number == 0:
+            number = "No number"
+        label_upper = Label(self, text=str(upper))
+        label_upper.grid(row=0)
+        label_lower = Label(self, text=str(lower))
+        label_lower.grid(row=0)
+        label_symbols = Label(self, text=str(sysmbols))
+        label_symbols.grid(row=0)
+        label_number = Label(self, text=str(number))
+        label_number.grid(row=0)
+             
         
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.pack()
         self.createWidgets()
+        
 
 root = Tk()
 root.geometry("450x450")
 root.title("Secret Enough? | The PSIT Project")
+
 app = Application(master=root)
 app.mainloop()
 
