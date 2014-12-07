15b191b17b53407400d09b33f1adc6619dc6c4b6
Apisit Seekaewnit <mos_max@hotmail.com>
2014-12-08 04:58:05 +0700

    Big Change Coding Round 2
    make it more OO and easy to create funtion

--- SecretEnough.py
+++ SecretEnough.py
@@ -1,66 +1,71 @@
-"""How was your password?"""
 from Tkinter import *
 
-class Application(Frame):
-    def createWidgets(self):
-        label_input = Label(self, text="Insert Password")
-        label_input.grid(row=1, column=0)
-        pass_input = Entry(self, bd=5)
-        pass_input.focus_set()
-        pass_input.grid(row=1, column=1)
-        btn_check = Button(self, text="Check it")
-        btn_check.grid(row=1, column=2)
-        btn_check["command"] = self.display(pass_input)
-        #cannot operate need to pass password input
-        #from pass_input to basic data
-    def basic_data(self, password): #calculate inside password
-        upper, lower, symbols, number = 0, 0, 0, 0
-        get_pass = password.get()
-        for element in get_pass:
-            if element.isalpha():
-                if element.isupper():
-                    upper += 1
-                else:
-                    lower += 1
-            elif element.isdigit():
-                number += 1
-            else:
-                symbols += 1
-        if upper == 0:
-            upper = "No upper case"
-        if lower == 0:
-            lower = "No lower case"
-        if symbols == 0:
-            symbols = "No symbols"
-        if number == 0:
-            number = "No number"
-        packed = [upper, lower, symbols, number]
-        return packed
-    
-    def display(self, password):
-            data_in = self.basic_data(password)
-            label_upper = Label(self, text=str(data_in[0]))
-            label_upper.grid(row=2, column=1)
-            label_lower = Label(self, text=str(data_in[1]))
-            label_lower.grid(row=3, column=1)
-            label_symbols = Label(self, text=str(data_in[2]))
-            label_symbols.grid(row=4, column=1)
-            label_number = Label(self, text=str(data_in[3]))
-            label_number.grid(row=5, column=1)
+class Welcome():
+    def __init__(self, master):
+        self.master = master
+        self.master.geometry('400x200')
+        self.master.title('Secret Enough?')
 
+        self.label1 = Label(self.master, text='Welcome to the Secret Enough? Application')
+        self.label1.grid(row=0, column=1)
+        self.lable2 = Label(self.master, text='Which funtion you want to use?')
+        self.lable2.grid(row=1, column=1)
+        self.button1 = Button(self.master, text='Identify Password', fg='blue', command=self.gotoCalPass)
+        self.button1.grid(row=6, column=0)
+        self.button2 = Button(self.master, text='Generate Password', fg='blue', command=self.gotoGePass)
+        self.button2.grid(row=6, column=1)
+        self.button3 = Button(self.master, text='Exit', fg='red', command=self.finish)
+        self.button3.grid(row=6, column=2)
+
+
+
+    def gotoCalPass(self):
+        root2 = Toplevel(self.master)
+        myGUI = IdenPass(root2)
+
+
+    def gotoGePass(self):
+        root3 = Toplevel(self.master)
+        myGUI = GenePass(root3)
     
-                  
-        
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.pack()
-        self.createWidgets()
+
+
+    def finish(self):
+        self.master.destroy()
         
 
-root = Tk()
-root.geometry("450x450")
-root.title("Secret Enough?")
 
-app = Application(master=root)
-app.mainloop()
+class IdenPass():
+    def __init__(self, master):
+        self.master = master
+        self.master.geometry('500x200')
+        self.master.title('Password Identify')
 
+        self.button = Button(self.master, text='Back', fg='red', command=self.back)
+        self.button.grid(row=0, column=0)
+        
+
+
+    def back(self):
+        self.master.destroy()
+
+class GenePass():
+    def __init__(self, master):
+        self.master = master
+        self.master.geometry('500x200')
+        self.master.title('Password Genereter')
+
+        self.button = Button(self.master, text='Back', fg='red', command=self.back)
+        self.button.grid(row=0, column=0)
+
+    def back(self):
+        self.master.destroy()
+
+
+def main():
+    root = Tk()
+    myGUIWelcome = Welcome(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
