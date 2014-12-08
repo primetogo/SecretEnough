5e6f3af985365506276c222f840da32bcd87eb46
Apisit Seekaewnit <mos_max@hotmail.com>
2014-12-08 22:01:37 +0700

    Simple IdenPass
    can do identify password function

--- SecretEnough.py
+++ SecretEnough.py
@@ -37,13 +37,66 @@ class Welcome():
 
 class IdenPass():
     def __init__(self, master):
+        self.text = StringVar()
         self.master = master
-        self.master.geometry('500x200')
+        self.master.geometry('600x200')
         self.master.title('Password Identify')
 
+        self.label = Label(self.master, text='This is a Password Identify Page')
+        self.label.grid(row=0, column=0)
+        self.label2 = Label(self.master, text='Please insert password that you want to identify')
+        self.label2.grid(row=1, column=0)
+        self.passtext = Entry(self.master, textvariable=self.text)
+        self.passtext.grid(row=1, column=2)
+        self.button = Button(self.master, text='OK', fg='blue', command=self.checkpass)
+        self.button.grid(row=1, column=3)
+
         self.button = Button(self.master, text='Back', fg='red', command=self.back)
-        self.button.grid(row=0, column=0)
+        self.button.grid(row=7, column=0)
+
+
+    def checkpass(self):
+        upper, lower, number, symbol = 0, 0, 0, 0
+        passtext = self.text.get()
+        for text in passtext:
+            if text.isupper():
+                upper += 1
+            elif text.islower():
+                lower += 1
+            elif text.isdigit():
+                number += 1
+            else:
+                symbol += 1
+        textlist = [upper, lower, number, symbol]
+        probanum = self.probaCrackPass(textlist)
+        self.labelresult = Label(self.master, text='Your password have :')
+        self.labelresult.grid(row=2, column=0)
+        self.labelupper = Label(self.master, text='%d Upper case' % upper)
+        self.labelupper.grid(row=3, column=0)
+        self.labellower = Label(self.master, text='%d Lower case' % lower)
+        self.labellower.grid(row=3, column=1)
+        self.labelnumber = Label(self.master, text='%d Digit' % number)
+        self.labelnumber.grid(row=3, column=2)
+        self.labelsymbol = Label(self.master, text='%d Symbol' % symbol)
+        self.labelsymbol.grid(row=3, column=3)
+        self.labelproba = Label(self.master, text='Probability to crack is : %.15f ' % probanum + ' %')
+        self.labelproba.grid(row=5, column=0)
+
         
+        
+    def probaCrackPass(self, textlist):
+        #print textlist
+        probanum = 0
+        upper = 26 ** textlist[0]  
+        lower = 26 ** textlist[1]  
+        number = 10 ** textlist[2]
+        symbol = 38 ** textlist[3]
+        probanum = upper * lower * number * symbol
+        probanum = 1.0 / (probanum / 100.0)
+        #print probanum
+        return probanum
+                                 
+            
 
 
     def back(self):
