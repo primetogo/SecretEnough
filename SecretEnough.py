1c64ce9be6339cae8ba49f1572565b3502453512
Panupong Prueksa <s5070089@kmitl.ac.th>
2014-12-14 01:00:59 +0700

    Remove and Fixing combination finding
    It should sum all not pow

--- SecretEnough.py
+++ SecretEnough.py
@@ -1,21 +1,23 @@
 from Tkinter import *
+from itertools import *
+from math import *
 
 class Welcome():
     def __init__(self, master):
         self.master = master
-        self.master.geometry('400x200')
+        self.master.geometry('400x100')
         self.master.title('Secret Enough?')
 
         self.label1 = Label(self.master, text='Welcome to the Secret Enough? Application')
-        self.label1.grid(row=0, column=1)
+        self.label1.grid(row=0, column=2)
         self.lable2 = Label(self.master, text='Which funtion you want to use?')
-        self.lable2.grid(row=1, column=1)
+        self.lable2.grid(row=1, column=2)
         self.button1 = Button(self.master, text='Identify Password', fg='blue', command=self.gotoCalPass)
-        self.button1.grid(row=6, column=0)
+        self.button1.grid(row=4, column=1, columnspan=1)
         self.button2 = Button(self.master, text='Generate Password', fg='blue', command=self.gotoGePass)
-        self.button2.grid(row=6, column=1)
+        self.button2.grid(row=4, column=2, columnspan=1)
         self.button3 = Button(self.master, text='Exit', fg='red', command=self.finish)
-        self.button3.grid(row=6, column=2)
+        self.button3.grid(row=4, column=3, columnspan=1)
 
 
 
@@ -38,6 +40,7 @@ class Welcome():
 class IdenPass():
     def __init__(self, master):
         self.text = StringVar()
+        self.force = IntVar()
         self.master = master
         self.master.geometry('600x200')
         self.master.title('Password Identify')
@@ -48,16 +51,20 @@ class IdenPass():
         self.label2.grid(row=1, column=0)
         self.passtext = Entry(self.master, textvariable=self.text)
         self.passtext.grid(row=1, column=2)
+        self.label3 = Label(self.master, text='Please insert speed of guessing')
+        self.label3.grid(row=2, column=0, sticky='W')
+        self.passforce = Entry(self.master, textvariable=self.force)
+        self.passforce.grid(row=2, column=2)
         self.button = Button(self.master, text='OK', fg='blue', command=self.checkpass)
-        self.button.grid(row=1, column=3)
-
+        self.button.grid(row=3, column=1)
         self.button = Button(self.master, text='Back', fg='red', command=self.back)
-        self.button.grid(row=7, column=0)
+        self.button.grid(row=3, column=0)
 
 
     def checkpass(self):
         upper, lower, number, symbol = 0, 0, 0, 0
         passtext = self.text.get()
+        force = self.force.get()
         for text in passtext:
             if text.isupper():
                 upper += 1
@@ -68,32 +75,32 @@ class IdenPass():
             else:
                 symbol += 1
         textlist = [upper, lower, number, symbol]
-        probanum = self.probaCrackPass(textlist)
+        probanum = self.probaCrackPass(textlist, force)
         self.labelresult = Label(self.master, text='Your password have :')
-        self.labelresult.grid(row=2, column=0)
+        self.labelresult.grid(row=4, column=0)
         self.labelupper = Label(self.master, text='%d Upper case' % upper)
-        self.labelupper.grid(row=3, column=0)
+        self.labelupper.grid(row=4, column=1)
         self.labellower = Label(self.master, text='%d Lower case' % lower)
-        self.labellower.grid(row=3, column=1)
+        self.labellower.grid(row=4, column=2)
         self.labelnumber = Label(self.master, text='%d Digit' % number)
-        self.labelnumber.grid(row=3, column=2)
+        self.labelnumber.grid(row=4, column=4)
         self.labelsymbol = Label(self.master, text='%d Symbol' % symbol)
-        self.labelsymbol.grid(row=3, column=3)
-        self.labelproba = Label(self.master, text='Probability to crack is : %.15f ' % probanum + ' %')
-        self.labelproba.grid(row=5, column=0)
+        self.labelsymbol.grid(row=4, column=5)
+        self.labeltextproba = Label(self.master, text='Your password has '+str(probanum)+' combination')
+        self.labeltextproba.grid(row=5, column=0)
 
         
         
-    def probaCrackPass(self, textlist):
-        #print textlist
+    def probaCrackPass(self, textlist, force):
         probanum = 0
-        upper = 26 ** textlist[0]  
-        lower = 26 ** textlist[1]  
-        number = 10 ** textlist[2]
-        symbol = 38 ** textlist[3]
-        probanum = upper * lower * number * symbol
-        probanum = 1.0 / (probanum / 100.0)
-        #print probanum
+        lst_cal = [26 ** textlist[0], 26 ** textlist[1], 10 ** textlist[2], 38 ** textlist[3]]
+        for element in lst_cal:
+            if element != 1:
+                probanum += element    
+        #Calculate time that use to crack password aka bruteforce
+        
+        print lst_cal
+        print probanum
         return probanum
                                  
             
